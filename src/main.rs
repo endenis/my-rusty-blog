4ca762583d4fd89c59a4e823de5690f0bd5a1795
endenis <endenis@gmail.com>
2014-11-21 23:59:15 +0100

    Added router and static files serving

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 extern crate nickel;
 
 use std::io::net::ip::Ipv4Addr;
-use nickel::{Nickel, Request, Response, HttpRouter};
+use nickel::{Nickel, Request, Response, HttpRouter, StaticFilesHandler};
 
 fn main() {
     let mut server = Nickel::new();
-    
+
+    let mut router = Nickel::router();
+
     fn index_page_handler (_request: &Request, response: &mut Response) { 
         response.send("Hello my-rusty-blog"); 
     }
 
-    server.get("/", index_page_handler);
+    router.get("/", index_page_handler);
+
+    server.utilize(router);
+    server.utilize(StaticFilesHandler::new("public/"));
     server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
